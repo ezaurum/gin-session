c58c8a4166865268070111089fd13e9d051af02c
ezaurum <ezaurum@gmail.com>
2017-12-25 17:37:03 +0700

    Cleanup flow

--- cookie/cookie.go
+++ cookie/cookie.go
@@ -8,6 +8,16 @@ import (
 	"log"
 )
 
+type cookieAuthenticator struct {
+	store                        ezs.Store
+	isInit                       bool
+	createCallback               gs.SessionHandlerFunc
+	activateCallback             gs.SessionHandlerFunc
+	cookieInvalidCallback        gs.SessionCookieHandlerFunc
+	createAuthenticationCallback gs.SessionHandlerFunc
+	loadAuthenticationCallback   gs.SessionCookieHandlerFunc
+}
+
 var _ gs.Authenticator = &cookieAuthenticator{}
 
 const (
@@ -15,6 +25,82 @@ const (
 	sessionIDCookieName      = "ca-default-name"
 	persistCookieName        = "ca-default-remember-me"
 )
+
+func (ca cookieAuthenticator) Handler() gin.HandlerFunc {
+	if !ca.isInit {
+		panic("Not initialized")
+	}
+
+	return func(c *gin.Context) {
+
+		session, needSession := ca.findSession(c)
+
+		if needSession {
+			session = ca.createSession(c)
+		}
+
+		//TODO 여기서 authenticate가 일어나야 한다
+		ca.authenticate(c, session)
+
+		//activate
+		ca.activateSession(c, session)
+	}
+}
+
+func (ca cookieAuthenticator) createSession(c *gin.Context) ezs.Session {
+	// set cookie
+	session := ca.store.GetNew()
+	//TODO 여기 duration 과 세션 duration 맞추기
+	c.SetCookie(sessionIDCookieName, session.ID(),
+		100000,
+		"/", "",
+		false, true)
+	// created
+	ca.createCallback(c, session)
+	return session
+}
+
+func (ca cookieAuthenticator) authenticate(c *gin.Context, session ezs.Session) {
+	//TODO 세션이 이미 authenticate 되었는지 체크해서 되었다면 안 건드려도 됨
+	pCookie, e := c.Cookie(persistCookieName)
+	if nil != e {
+		if nil != ca.createAuthenticationCallback {
+			ca.createAuthenticationCallback(c, session)
+		}
+	} else {
+		// load user with cookie
+		if nil != ca.loadAuthenticationCallback {
+			ca.loadAuthenticationCallback(c, session, pCookie)
+		}
+	}
+}
+
+func (ca cookieAuthenticator) activateSession(c *gin.Context, session ezs.Session) {
+	//TODO gin에 종속된 부분은 여기다
+	c.Set(DefaultSessionContextKey, session)
+	ca.activateCallback(c, session)
+}
+
+func (ca cookieAuthenticator) findSession(c *gin.Context) (ezs.Session, bool) {
+	sessionIDCookie, e := c.Cookie(sessionIDCookieName)
+	needSession := nil != e
+	var session ezs.Session
+	var noSession bool
+	if nil == e {
+		//TODO secure
+		s, n := ca.store.Get(sessionIDCookie)
+		session = s
+		noSession = !n
+
+		if noSession {
+			// 세션 유효하지 않은 경우
+			// 만료되었거나, 값 조작이거나
+			ca.cookieInvalidCallback(c, nil, sessionIDCookie)
+		}
+	}
+	needSession = needSession || noSession
+	return session, needSession
+}
 
 func Default() gs.Authenticator {
 	return New(memstore.Default())
@@ -35,16 +121,6 @@ func New(store ezs.Store) gs.Authenticator {
 		activateCallback:             logF0,
 		createCallback:               logF0,
 	}
-}
-
-type cookieAuthenticator struct {
-	store                        ezs.Store
-	isInit                       bool
-	createCallback               gs.SessionHandlerFunc
-	activateCallback             gs.SessionHandlerFunc
-	cookieInvalidCallback        gs.SessionCookieHandlerFunc
-	createAuthenticationCallback gs.SessionHandlerFunc
-	loadAuthenticationCallback   gs.SessionCookieHandlerFunc
 }
 
 func (ca *cookieAuthenticator) Callbacks(create gs.SessionHandlerFunc,
@@ -71,73 +147,9 @@ func (ca *cookieAuthenticator) Callbacks(create gs.SessionHandlerFunc,
 	return ca
 }
 
-func (ca cookieAuthenticator) Handler() gin.HandlerFunc {
-	if !ca.isInit {
-		panic("Not initialized")
-	}
-
-	return func(c *gin.Context) {
-
-		session, needSession := ca.getSession(c)
-
-		if needSession {
-			// set cookie
-			session = ca.store.GetNew()
-			//TODO 여기 duration 과 세션 duration 맞추기
-			c.SetCookie(sessionIDCookieName, session.ID(),
-				100000,
-				"/", "",
-				false, true)
-
-			// created
-			ca.createCallback(c, session)
-		}
-
-		//TODO 여기서 authenticate가 일어나야 한다
-		ca.authenticate(c, session)
-
-		//activate
-		//TODO gin에 종속된 부분은 여기다
-		c.Set(DefaultSessionContextKey, session)
-
-		ca.activateCallback(c, session)
-	}
-}
-
-func (ca cookieAuthenticator) getSession(c *gin.Context) (ezs.Session, bool) {
-	sessionIDCookie, e := c.Cookie(sessionIDCookieName)
-	needSession := nil != e
-	var session ezs.Session
-	var noSession bool
-	if nil == e {
-		//TODO secure
-		s, n := ca.store.Get(sessionIDCookie)
-		session = s
-		noSession = !n
-
-		if noSession {
-			// 세션 유효하지 않은 경우
-			// 만료되었거나, 값 조작이거나
-			ca.cookieInvalidCallback(c, nil, sessionIDCookie)
-		}
-	}
-	needSession = needSession || noSession
-	return session, needSession
-}
-
-func (ca cookieAuthenticator) authenticate(c *gin.Context, session ezs.Session) {
-	//TODO 세션이 이미 authenticate 되었는지 체크해서 되었다면 안 건드려도 됨
-	pCookie, e := c.Cookie(persistCookieName)
-	if nil != e {
-		if nil != ca.createAuthenticationCallback {
-			ca.createAuthenticationCallback(c, session)
-		}
-	} else {
-		// load user with cookie
-		if nil != ca.loadAuthenticationCallback {
-			ca.loadAuthenticationCallback(c, session, pCookie)
-		}
-	}
+func (ca *cookieAuthenticator) Init() gs.Authenticator {
+	ca.isInit = true
+	return ca
 }
 
 func DefaultCookieInvalid(c *gin.Context, s ezs.Session, cookie string) {
@@ -150,9 +162,4 @@ func DefaultCookieInvalid(c *gin.Context, s ezs.Session, cookie string) {
 	// 리다이렉트로 처리
 	//TODO c.Redirect(http.StatusFound, c.Request.URL.String())
 	//TODO c.Abort()
-}
-
-func (ca *cookieAuthenticator) Init() gs.Authenticator {
-	ca.isInit = true
-	return ca
 }
